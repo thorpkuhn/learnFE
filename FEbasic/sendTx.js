1deab85f886d0abfb004f0414c15ac5367b20976
Cryptopragmatic <tristan.alberio@gmail.com>
2022-09-06 12:14:55 +0800

    finally deployed the contract

--- FEbasic/sendTx.js
+++ FEbasic/sendTx.js
@@ -17,7 +17,7 @@ const nonce = await alchemy.core.getTransactionCount(wallet.address, "latest");
 let transaction = {
   to: "0x31B98D14007bDEe637298086988A0bBd31184523", // faucet address to return eth
   value: 10,
-  gasLimit: "21000",
+  gasLimit: "201000",
   maxFeePerGas: "20000000000",
   nonce: nonce,
   type: 2,
